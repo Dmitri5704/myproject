24e3ab7cb39176bb8ae3b986e88f6954aef0fd8d
unknown <v1rrr@rambler.ru>
2022-07-27 18:38:00 +0300

    third les

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const numberOfFilms = prompt("Сколько фильмов вы посмотрели?", "");
+const numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", "");
 const personalMovieDB = {
     count:numberOfFilms,
     movies:{},
@@ -8,16 +8,29 @@ const personalMovieDB = {
 };
 
 
-      for(let i = 0; i<2; i++){
-       const  a=prompt("Один из последних просмотренных фильмов", ''),
-              b=prompt("Наколько оцените баллов его?", '');
+      for(let i = 0; i<2; i++) {
+        const  a=prompt('Один из последних просмотренных фильмов', ''),
+               b=prompt('Наколько оцените баллов его?', '');
         personalMovieDB.movies[a]=b;
-        }
-        console.log(personalMovieDB);
-       
+              if(personalMovieDB.count<10){
+                console.log('Мало посмотрел');
+              }else if(personalMovieDB.count>=10 && personalMovieDB.count<30){
+                console.log("Красавчик");
+              }else if(personalMovieDB.count>=30){
+                console.log("кономан");
+              }else{
+                 console.log("ошибка");
+              }
+               
+     }
+        
+        
+console.log(personalMovieDB);
+              
+              
         
 
        
       
       
-      
+     
